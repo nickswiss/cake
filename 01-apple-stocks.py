16bc45d66c7297629ac8feb8ab64443e69d3d365
Nick Arnold <nick.arnold@dealertrack.com>
2017-04-28 16:28:02 -0400

    removed bad formatting

--- 01-apple-stocks.py
+++ 01-apple-stocks.py
@@ -1,15 +1,15 @@
 def get_max_profits(stock_prices):
-    
     if len(stock_prices) < 2:
         raise IndexError('Profit requires at least two prices')
-    
+
     min_price = stock_prices[0]
     max_profit = stock_prices[1] - stock_prices[0]
-    
+
     for index, price in enumerate(stock_prices):
-         if index == 0:
+        if index == 0:
             continue
-        max_profit = max(potential_max, max_profit)
+
+        max_profit = max(price - min_price, max_profit)
         min_price = min(min_price, price)
         
     return max_profit
